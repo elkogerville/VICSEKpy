384900620b9b85f12d9f42d440bbe96e848de373
Elko <129769199+elkogerville@users.noreply.github.com>
2023-07-17 04:26:43 -0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+     name = "VICSEKpy",
+     version = "0.1",
+     author = "Elko Gerville-Reache",
+     author_email = "elko.gerville-reache@yale.edu",
+     license='MIT',
+     description = "python program that performs 3d active matter simulation based on vicsek model",
+     packages = ["VICSEKpy"]
+)
